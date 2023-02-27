1ca67d6c45d7f57490b5f0d334ea3157e479b0d4
Angelbidas <87902726+Angelbidas@users.noreply.github.com>
2023-02-27 09:30:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -109,64 +109,6 @@ window.addEventListener('load',()=>{
     })
       .then(response => response.json())
       .then(data => {
-        // Create task element for the new task
-        const task_el = document.createElement("div");
-        task_el.classList.add("task");
-
-        const task_content_el = document.createElement("div");
-        task_content_el.classList.add("content");
-
-        task_el.appendChild(task_content_el);
-
-        const task_input_el = document.createElement("input");
-        task_input_el.classList.add("text");
-        task_input_el.type = "text";
-        task_input_el.value = data.name;
-        task_input_el.setAttribute("readonly","readonly");
-
-        task_content_el.appendChild(task_input_el);
-
-        const task_actions_el = document.createElement("div");
-        task_actions_el.classList.add("actions");
-
-        const task_edit_el = document.createElement("button");
-        task_edit_el.classList.add("edit");
-        task_edit_el.innerHTML = "Edit";
-    
-        const task_delete_el = document.createElement("button");
-        task_delete_el.classList.add("delete");
-        task_delete_el.innerHTML = "Delete";
-    
-        task_actions_el.appendChild(task_edit_el);
-        task_actions_el.appendChild(task_delete_el);
-    
-        task_el.appendChild(task_actions_el);
-        list_el.appendChild(task_el);
-
-        task_edit_el.addEventListener('click',()=>{
-          if(task_edit_el.innerText.toLowerCase()=='edit'){
-            task_input_el.removeAttribute("readonly");
-            task_input_el.focus();
-            task_edit_el.innerText="Save";
-          } else{
-            task_input_el.setAttribute("readonly","readonly");
-            task_edit_el.innerText="Edit";
-          }
-        });
-    
-        task_delete_el.addEventListener('click',()=>{
-          list_el.removeChild(task_el);
-          fetch('db.json', {
-            method: 'DELETE',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(newTask)
-          })
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => console.log(error));
-        });
         input.value = "";
       })
       .catch(error => console.log(error));
